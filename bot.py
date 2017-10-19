b04462bb839e1c5cfe8f512e0526d68c1d13dc62
theDeimus <jack.limberis@gmail.com>
2017-10-19 11:35:21 +0300

    My address added

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-    updater.bot.set_webhook("https://python-telegram-webhook-jamaat.herokuapp.com/" + TOKEN)
+    updater.bot.set_webhook("https://ablebot.herokuapp.com/" + TOKEN)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
